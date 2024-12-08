4566954806fda11d36bc7514ad2e8ffbdcc3e2c6
강건우 <rkdrjsdn1000@gmail.com>
2024-12-08 17:23:36 +0900

    수정

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.use(morgan('combined', {
 app.use(errorMiddleware);
 
 // 서버 시작
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 443;
 app.listen(PORT, () => {
   console.log(`서버가 ${PORT}번 포트에서 실행 중입니다.`);
 });
